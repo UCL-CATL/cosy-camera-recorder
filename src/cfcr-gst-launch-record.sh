0ff863cb24d82864c94bb7b6a5aeb34965aee9d5
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2016-12-05 17:22:39 +0100

    gst-launch: encode in mpeg4
    The same format as the videos recorded by Pupil Capture.

--- src/cfcr-gst-launch-record.sh
+++ src/cfcr-gst-launch-record.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gst-launch-1.0 v4l2src num-buffers=50 ! queue ! x264enc ! mp4mux ! filesink location=video.mp4
+gst-launch-1.0 v4l2src num-buffers=50 ! queue ! avenc_mpeg4 ! mp4mux ! filesink location=video.mp4
