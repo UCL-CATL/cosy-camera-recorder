4edb4c18a36c093926606a20780ea0da11be9c63
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2017-01-10 15:41:22 +0100

    Add test for sending start/stop via ZeroMQ
    The port will be 6001. For cosy-pupil-server's external recorder it is
    6000.

---
+++ tests/test-request.c
@@ -0,0 +1,44 @@
+/* Request-reply test */
+
+#include <zmq.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "zhelpers.h"
+
+int
+main (void)
+{
+	void *context;
+	void *requester;
+	char *request;
+	char *reply;
+	int i;
+
+	context = zmq_ctx_new ();
+
+	requester = zmq_socket (context, ZMQ_REQ);
+	zmq_connect (requester, "tcp://localhost:6001");
+
+	request = "start";
+	printf ("Send request: %s ...\n", request);
+	zmq_send (requester, request, strlen (request), 0);
+
+	reply = s_recv (requester);
+	printf ("Reply received: %s\n", reply);
+	free (reply);
+
+	sleep (10);
+
+	request = "stop";
+	printf ("Send request: %s ...\n", request);
+	zmq_send (requester, request, strlen (request), 0);
+
+	reply = s_recv (requester);
+	printf ("Reply received: %s\n", reply);
+	free (reply);
+
+	zmq_close (requester);
+	zmq_ctx_destroy (context);
+	return 0;
+}
